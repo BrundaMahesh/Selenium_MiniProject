122487a0ea2e3d50dac166dba79892c7870ee3da
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-16 21:32:01 +0530

    Add project files.

---
+++ MakeMyTrip/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MakeMyTrip
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
