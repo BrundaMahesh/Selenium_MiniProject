f55335d1a5663fce62dbd28dd353e642d2b18a7c
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-23 10:20:14 +0530

    created new folders

---
+++ MakeMyTrip/TestScripts/UserManagementTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeMyTrip.TestScripts
+{
+    [TestFixture]
+    internal class UserManagementTests
+    {
+        [Test]
+        public void BookFlightTest()
+        {
+
+        }
+    }
+}
